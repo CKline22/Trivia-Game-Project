e9a0fc185a18f222d2cd15c86a39b17cbc313ee4
CKline22 <CKline.1022@gmail.com>
2022-03-09 18:25:09 -0500

    js inprogress

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-//const variables document.getElementById//
-
+//const variables
+const startButton = document.getElementById('start-button')
+const nextButton = document.getElementById('next-button')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
+const scoreElement = document.getElementById('score')
+let shuffledQuestions, currentQuestionIndex
+let score = 0;
 
 //startgame functiion and score reset//
-
-//event listners for buttons
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = question.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    questionElement.classList.remove('hide')
+    scoreElement.classList.remove('hide')
+    score = 0
+    scoreElement.innerText = "Score: " + score
+    setNextQuestion()
+}
 
 //next question function//
+function setNextQuestion(){
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-//show question function//
+//event listners for buttons
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+
+//show question function  //
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('button')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonElement.appendChild(button)
+    })
+}
 
 //select answers fucntion and score++//
 
