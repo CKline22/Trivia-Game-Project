392049be493bd5b4c1662bb8b443503b4bb8554c
CKline22 <CKline.1022@gmail.com>
2022-03-09 17:46:23 -0500

    starting js

---
+++ script.js
@@ -0,0 +1,14 @@
+//const variables document.getElementById//
+
+
+//startgame functiion and score reset//
+
+//event listners for buttons
+
+//next question function//
+
+//show question function//
+
+//select answers fucntion and score++//
+
+//questions array//
